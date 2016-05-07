baf044cc7b16a07f28787ced45831b3baa83dc6d
Moressette <wangcong9614@hotmail.com>
2016-05-07 17:41:04 +0800

    Already add dictionary database,check success!

---
+++ OneWord/OneWord/MyPlayground.playground/Contents.swift
@@ -0,0 +1,8 @@
+//: Playground - noun: a place where people can play
+
+import Cocoa
+
+var str = "Hello, playground"
+print(str)
+let data = db.query("select Word from words where ID=13")
+print(data)
--- OneWord/OneWord/ViewController.swift
+++ OneWord/OneWord/ViewController.swift
@@ -12,26 +12,36 @@ class ViewController: UIViewController {
     
     var db:SQLiteDB!
     
-    @IBOutlet var txtUname: UITextField!
-    @IBOutlet var txtMobile: UITextField!
     
+    @IBOutlet weak var showWord: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
-        
         //获取数据库实例
         db = SQLiteDB.sharedInstance()
         //如果表还不存在则创建表（其中uid为自增主键）
-        db.execute("create table if not exists t_user(uid integer primary key,uname varchar(20),mobile varchar(20))")
+        //db.execute("create table if not exists t_user(uid integer primary key,uname varchar(20),mobile varchar(20))")
         //如果有数据则加载
-        initUser()
+        var dta = db.query("select Word from words where ID=13")
+        let row = dta[0]
+        if var words = row["Word"] {
+            showWord.text = words as? String
+        }
+        
+        //showWord.text = "123"
+        //showWord.text = data as! String
+        /*if data.count > 0{
+            //let words = data[data.count-1]
+            showWord.text = data as? String
+            
+        }*/
     }
     
-    //点击保存
-    @IBAction func saveClicked(sender: AnyObject) {
-        saveUser()
-    }
+
     
-    //从SQLite加载数据
+    
+
+
+    /*//从SQLite加载数据
     func initUser() {
         let data = db.query("select * from t_user")
         if data.count > 0 {
@@ -40,19 +50,7 @@ class ViewController: UIViewController {
             txtUname.text = user["uname"] as? String
             txtMobile.text = user["mobile"] as? String
         }
-    }
-    
-    //保存数据到SQLite
-    func saveUser() {
-        let uname = self.txtUname.text!
-        let mobile = self.txtMobile.text!
-        //插入数据库，这里用到了esc字符编码函数，其实是调用bridge.m实现的
-        let sql = "insert into t_user(uname,mobile) values('\(uname)','\(mobile)')"
-        print("sql: \(sql)")
-        //通过封装的方法执行sql
-        let result = db.execute(sql)
-        print(result)
-    }
+    }*/
     
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
