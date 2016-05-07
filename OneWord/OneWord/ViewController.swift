0242dad6e41fa25447e7939f84627fe62e3fd20c
Moressette <wangcong9614@hotmail.com>
2016-05-07 17:54:39 +0800

    Add a button.When button be tapped,change the word.Check success!

--- OneWord/OneWord/ViewController.swift
+++ OneWord/OneWord/ViewController.swift
@@ -14,6 +14,8 @@ class ViewController: UIViewController {
     
     
     @IBOutlet weak var showWord: UILabel!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         //获取数据库实例
@@ -21,7 +23,7 @@ class ViewController: UIViewController {
         //如果表还不存在则创建表（其中uid为自增主键）
         //db.execute("create table if not exists t_user(uid integer primary key,uname varchar(20),mobile varchar(20))")
         //如果有数据则加载
-        var dta = db.query("select Word from words where ID=13")
+        var dta = db.query("select Word from words where ID=1")
         let row = dta[0]
         if var words = row["Word"] {
             showWord.text = words as? String
@@ -36,8 +38,17 @@ class ViewController: UIViewController {
         }*/
     }
     
-
-    
+    var id = 2
+    @IBAction func okTapped(sender: AnyObject) {
+        //Insert code here... :)
+        var words = db.query("select Word from words where ID=?" , parameters:[id])
+        let row = words[0]
+        if var w = row["Word"]{
+            showWord.text = w as? String
+        }
+        id++
+    }
+   
     
 
 
