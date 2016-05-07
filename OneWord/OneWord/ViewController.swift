61064aa0b787121661d32102c5be2fab2618f144
Moressette <wangcong9614@hotmail.com>
2016-05-07 10:30:53 +0800

    5/7 create a new project

---
+++ OneWord/OneWord/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  OneWord
+//
+//  Created by 王聪 on 16/5/7.
+//  Copyright © 2016年 TeamRandom. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
