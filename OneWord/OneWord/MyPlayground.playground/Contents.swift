7e84434b9fd6a22d7e73d531803beb02ebaeeb27
Moressette <wangcong9614@hotmail.com>
2016-05-08 09:33:12 +0800

    New Check.All SUCCESS!

--- OneWord/OneWord/MyPlayground.playground/Contents.swift
+++
@@ -1,8 +0,0 @@
-//: Playground - noun: a place where people can play
-
-import Cocoa
-
-var str = "Hello, playground"
-print(str)
-let data = db.query("select Word from words where ID=13")
-print(data)
